30dfa7b9db3ae9ef66351924ce06a79bcd97d89b
Victor Morei <mcore91@yandex.ru>
2021-02-18 09:10:18 +0200

    Main Page Layout

--- tumweb/src/index.js
+++ tumweb/src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Layout, Menu, Input } from 'antd';
+
+const { Header, Content, Footer } = Layout;
+const { TextArea } = Input;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Layout>
+    <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+        <Menu.Item key="1">Page 1</Menu.Item>
+        <Menu.Item key="2">Page 2</Menu.Item>
+        <Menu.Item key="3">Page 3</Menu.Item>
+      </Menu>
+    </Header>
+    <Content className="site-layout" style={{ padding: '0 50px', marginTop: 128 }}>
+      <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
+        <TextArea rows={15} />
+      </div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>Morei Victor © 2021, Web Project, TUM</Footer>
+  </Layout>,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
